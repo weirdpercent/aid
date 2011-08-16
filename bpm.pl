05d65e15004714007b716e71a986231beebeb289
Drew <weirdpercent@gmail.com>
2011-08-15 21:43:11 -0400

    BASED ON FEEDBACK FROM PERLMONKS.ORG I MADE THESE MINOR CHANGES TO BOTH FILES

--- bpm.pl
+++ bpm.pl
@@ -2,44 +2,37 @@
 ########################################
 #             A. I. D. v1.0            #
 #        database search script        #
-#           coded by bitsmart          #
-#       (c) 2000 Andrew Prentice       #
-#         bitsmart@bitsmart.org        #
+#           coded by weirdpercent      #
+#         (c) 2011 Drew Prentice       #
+#          weirdpercent@gmail.com      #
 ########################################
 print "\nThis program searches for songs with the same BPM.\n";
 print "\nWhat BPM do you want to search for? ";
 chomp($inpt = <STDIN>); #get the BPM to search for
 print "searching..."; #make it look like we're doing something
 $bpm = "BPM=$inpt"; #add our little tag
-opendir(DIREC,".") || die "cannot open directory"; #open current directory w/ error handling
-foreach $name(sort(readdir(DIREC))) { #for each filename beginning w/ "BPM-", add to array
-	if ($name =~ /^BPM-/) { #if the name contains "BPM-"...
-		@files = (@files, $name); #add it!
-	}
-	print "."; #add another "doing something" mark
+opendir(DIREC,".") || die "cannot open directory"; #open current direc
++tory w/ error handling
+foreach $name(sort(readdir(DIREC))) { #for each filename beginning w/ 
++"BPM-", add to array
+    if ($name =~ /^BPM-/) { #if the name contains "BPM-"...
+        push @files, $name; #add it!
+    }
+    print "."; #add another "doing something" mark
 }
 closedir(DIREC); #close the dir
-foreach $filename (@files) { #for each filename in the array
-	chomp($filename); #chomp trailing newline
-	open(PHILE, "$filename") || die "cannot open $filename"; #open file w/ error handling
-	@lines = <PHILE>; #put the lines of the file into an array
-	if ($lines[$linenumber]=~/$bpm/) { #if any line in the array had $bpm,
-		$linenumber++; #increment the line counter
-		$flag = "yes"; #turn the "bpm flag" on
-	} else {
-		$flag = "no"; #don't turn on the flag
-	}
-	print "\n"; #clean up
-	if ($flag = "yes") { #if the flag is on
-		print "\n$lines[0]\n"; #print the Artist/Album info
-		$linenumber = 2; #reset the line counter
-		foreach (@lines) { #for each line in the array
-			if ($_=~/$bpm/) { #if $bpm is found,
-				print; #print that line
-			}
-		}
-	} else {
-		print "sorry, no matching BPMs found."; #deliver the bad news
-	}
-	close(PHILE); #finish up
+foreach my $filename ( @files ) { #for each filename in the array
+    open(PHILE, '<', $filename) || die "cannot open $filename: $!"; #o
++pen file w/ error handling
+    my @lines = scalar <PHILE>; #get the Artist/Album info
+    while ( <PHILE> ) {
+        push @lines, $_ if /$bpm/;
+    }
+    if ( @lines > 1 ) {
+        print @lines;
+    }
+    else {
+        print "sorry, no matching BPMs found."; #deliver the bad news
+    }
+    close PHILE; #finish up
 }
